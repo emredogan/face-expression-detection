5e171e02651ca5335a7de93c14e4f27ff713ad28
Emre Dogan <emre.dogan@shortcut.no>
2023-07-19 09:47:27 +0200

    Initial Commit

---
+++ FaceExpressionsARKit/FaceExpressionsARKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceExpressionsARKitApp.swift
+//  FaceExpressionsARKit
+//
+//  Created by Emre Dogan on 19/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct FaceExpressionsARKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
